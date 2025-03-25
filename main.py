01e172b5602155c152fde9f48feb88d03ad21307
demch <md2710@ukr.net>
2025-03-25 23:52:01 +0200

    Збереження змін перед створенням нової гілки

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-import sys
-import time
+import psycopg2
 
-print(sys.argv)
+PGHOST = "ep-square-hill-a23rxcu8-pooler.eu-central-1.aws.neon.tech"
+PGDATABASE = "neondb"
+PGUSER = "neondb_owner"
+PGPASSWORD = "npg_hTqeb9Za1VLi"
+PORT = 5432
 
-for i in range(500):
-    print(i)
-    time.sleep(1)
+with psycopg2.connect(
+    dbname=PGDATABASE, user=PGUSER, password=PGPASSWORD, host=PGHOST, port=PORT
+) as connection:
+    with connection.cursor() as cursor:
+        query = """
+            CREATE TABLE IF NOT EXISTS brand (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) UNIQUE NOT NULL
+            )
+        """
+        cursor.execute(query)
+
+        query = """
+            CREATE TABLE IF NOT EXISTS customer (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS car (
+                id SERIAL PRIMARY KEY,
+                model VARCHAR(50) NOT NULL,
+                cost INTEGER,
+                brand_id INTEGER REFERENCES brand(id),
+                customer_id INTEGER REFERENCES customer(id)
+            );
+            """
+        cursor.execute(query)
